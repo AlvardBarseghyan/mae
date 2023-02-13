792440ae4c30d2bbdd0dd35e7a1b4f139708399b
AlvardBarseghyan <barseghyan.at@gmail.com>
2023-02-13 22:47:01 +0400

    argparse for training, evaluation py

--- eval.py
+++ eval.py
@@ -1,55 +1,17 @@
-import os 
-import torch 
+import os
+import torch
 import argparse
-import pytorch_lightning as pl
+from tqdm import tqdm
+from torchmetrics.functional import pairwise_cosine_similarity as cos_dist
 
-from loss import ContrastiveLoss
 from dataset import MAEDataset
+from pl_train import LightningMAE
+
 import models_mae
-
-# TODO: integrate aim
-
-class LightningMAE(pl.LightningModule):
-    def __init__(self, model, l1=0.5, lr=1e-4, num_classes=5, margin=1):
-        super().__init__()
-        self.model_mae = model
-        self.num_classes = num_classes
-        self.l1 = l1
-        self.lr = lr
-        self.criterion = ContrastiveLoss(num_classes=num_classes, margin=margin)
-        # self.save_hyperparameters(ignore=['model'])
-
-    def training_step(self, batch, batch_idx):
-
-        img = torch.einsum('nhwc->nchw', batch['image'])
-        print("Getting image embeddings...")
-        img_enc, mask, _ = self.model_mae.forward_encoder(img.float(), mask_ratio=0)
-        img_enc = img_enc[:, 1:, :].reshape(-1, img_enc.shape[-1])
-        print("Loss counting...")
-        loss = self.criterion(img_enc, batch['indices_labels'].reshape(-1))
-        total_loss = loss[0] + self.l1 * loss[1]
-        self.log('train_loss', total_loss)
-        print(f'Iter: {batch_idx}, pos_loss: {loss[0].item()}, neg_loss = {self.l1} * {loss[1].item()}, loss: {total_loss.item()}')
-
-        return total_loss
-
-
-    def configure_optimizers(self):
-        optimizer = torch.optim.Adam(self.model_mae.parameters(), lr=self.lr)
-        return optimizer
-
-
-    def forward(self, img):
-        encoded_image, mask, indices = self.model_mae.forward_encoder(img, mask_ratio=0)
-        return encoded_image[:, 1:, :]
-
-
 
 def main():
 
-    LEARNING_RATE = 1e-4
-    L1 = 1
-    parser = argparse.ArgumentParser(description='MAE -> Segmentation task')
+    parser = argparse.ArgumentParser(description='MAE -> Segmentation task: patch level evaluation')
     parser.add_argument(
         "--dataset_name",
         type=str,
@@ -58,11 +20,6 @@ def main():
     parser.add_argument(
         '--batch_size',
         default=20,
-        type=int,
-    )
-    parser.add_argument(
-        '--epochs',
-        default=100,
         type=int,
     )
     parser.add_argument(
@@ -86,11 +43,6 @@ def main():
         default='c9',
         help='available server names: c9, go',
     )
-    parser.add_argument(
-        '--random_init',
-        default=False,
-        type=bool,
-    )
 
     args = parser.parse_args()
 
@@ -102,6 +54,13 @@ def main():
         dataset = MAEDataset(path_ann, path_imgs, intersection_threshold=args.intersection_threshold, resize_image=True)
         dataloader = torch.utils.data.DataLoader(dataset, batch_size=args.batch_size, shuffle=True, num_workers=4)
         num_classes = 5
+
+        root_val = '/mnt/2tb/hrant/FAIR1M/fair1m_1000/val1000/'
+        path_ann = os.path.join(root_val, 'few_shot_8.json')
+        path_imgs = os.path.join(root_val, 'images')
+
+        dataset_val = MAEDataset(path_ann, path_imgs, resize_image=True)
+        dataloader_val = torch.utils.data.DataLoader(dataset_val, batch_size=5, shuffle=False)
 
     elif args.dataset_name.lower() == 'vis_drone' or args.dataset_name.lower() == 'vs':
         root = '/lwll/development/vis_drone/vis_drone_full/train/'
