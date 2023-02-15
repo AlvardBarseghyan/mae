f91ff15f94fe45cf4ecc594700d12303ee69f343
AlvardBarseghyan <barseghyan.at@gmail.com>
2023-02-15 16:25:43 +0400

    evaluation of datasets: todo: img representation

--- eval.py
+++ eval.py
@@ -2,6 +2,7 @@ import os
 import torch
 import argparse
 from tqdm import tqdm
+import pytorch_lightning as pl
 from torchmetrics.functional import pairwise_cosine_similarity as cos_dist
 
 from dataset import MAEDataset
@@ -9,8 +10,49 @@ from pl_train import LightningMAE
 
 import models_mae
 
-def main():
 
+def count_reference(dataloader, model, device, num_classes=5):
+    # model.eval()
+    reference_sum = torch.zeros((num_classes+1, 1024)).to(device)
+    counts = torch.zeros(num_classes+1).to(device)
+    for i, ds in tqdm(enumerate(dataloader), total=len(dataloader)):
+        img = torch.einsum('nhwc->nchw', ds['image']).to(device)
+        img_enc, _, _ = model.forward_encoder(img.float(), mask_ratio=0)
+        img_enc = img_enc[:, 1:, :].reshape(-1, img_enc.shape[-1])
+        index_labels = ds['indices_labels'].reshape(-1).to(device)
+        for j in torch.unique(torch.tensor(index_labels, dtype=int).clone().detach()):
+            indices = (index_labels == j).nonzero()
+            counts[j] += indices.shape[0]
+            new = img_enc[indices].reshape(-1, img_enc.shape[-1])
+            # print('class:', j, 'sum:', new.shape, 'shape:', new.sum(dim=0).shape)
+            reference_sum[j] += new.sum(dim=0).clone().detach()
+            
+    return reference_sum, counts
+
+def return_img_embed(dataloader, model):
+    # model.eval()
+    output = []
+    images = []
+    labels = []
+    for i, ds in tqdm(enumerate(dataloader), total=len(dataloader)):
+        img = torch.einsum('nhwc->nchw', ds['image'])
+        img_enc, _, _ = model.forward_encoder(img.float(), mask_ratio=0)
+        img_enc = img_enc[:, 1:, :] #.reshape(-1, img_enc.shape[-1])
+#         img_enc = model(img.float()) #.reshape(-1)
+        index_labels = ds['indices_labels'] #.reshape(-1)
+        labels.append(index_labels)
+        images.append(img.detach())
+        output.append(img_enc.detach())
+
+            
+    output = torch.cat(output, dim=0)
+    images = torch.cat(images, dim=0)
+    labels = torch.cat(labels, dim=0)
+    return (output, images, labels)
+
+def main():
+    LEARNING_RATE = 1e-4
+    L1 = 1
     parser = argparse.ArgumentParser(description='MAE -> Segmentation task: patch level evaluation')
     parser.add_argument(
         "--dataset_name",
@@ -43,36 +85,48 @@ def main():
         default='c9',
         help='available server names: c9, go',
     )
-
+    parser.add_argument(
+        '--annotation_train'
+    )
+    parser.add_argument(
+        '--annotation_val'
+    )
     args = parser.parse_args()
 
 
     if args.dataset_name.lower() == 'fair1m':
         root = '/mnt/2tb/hrant/FAIR1M/fair1m_1000/train1000/'
-        path_ann = os.path.join(root, 'few_shot_8.json')
+        path_ann = os.path.join(root, args.annotation_train)
         path_imgs = os.path.join(root, 'images')
         dataset = MAEDataset(path_ann, path_imgs, intersection_threshold=args.intersection_threshold, resize_image=True)
         dataloader = torch.utils.data.DataLoader(dataset, batch_size=args.batch_size, shuffle=True, num_workers=4)
         num_classes = 5
 
         root_val = '/mnt/2tb/hrant/FAIR1M/fair1m_1000/val1000/'
-        path_ann = os.path.join(root_val, 'few_shot_8.json')
+        path_ann = os.path.join(root_val, args.annotation_val)
         path_imgs = os.path.join(root_val, 'images')
 
         dataset_val = MAEDataset(path_ann, path_imgs, resize_image=True)
-        dataloader_val = torch.utils.data.DataLoader(dataset_val, batch_size=5, shuffle=False)
+        dataloader_val = torch.utils.data.DataLoader(dataset_val, batch_size=args.batch_size, shuffle=False)
 
     elif args.dataset_name.lower() == 'vis_drone' or args.dataset_name.lower() == 'vs':
         root = '/lwll/development/vis_drone/vis_drone_full/train/'
-        path_ann = os.path.join('./annotations/', 'few_shot_visdrone.json')
+        path_ann = os.path.join('./annotations/', args.annotation_train)
         path_imgs = os.path.join(root, '')
 
         dataset = MAEDataset(path_ann, path_imgs, intersection_threshold=args.intersection_threshold, resize_image=True)
         dataloader = torch.utils.data.DataLoader(dataset, batch_size=args.batch_size, shuffle=True, num_workers=4)
         num_classes = 11
 
+        root_val = '/lwll/development/vis_drone/vis_drone_full/train/'
+        path_ann = os.path.join('./annotations/', args.annotation_val)
+        path_imgs = os.path.join(root_val, '')
+
+        dataset_val = MAEDataset(path_ann, path_imgs, resize_image=True)
+        dataloader_val = torch.utils.data.DataLoader(dataset_val, batch_size=args.batch_size, shuffle=False)
+
     elif args.dataset_name.lower() == 'nightowls':
-        path_ann = './annotations/few_shot_8_nightowls.json'
+        path_ann = f'./annotations/{args.annotation_train}'
         path_imgs = '/home/ani/nightowls_stage_3/'
         dataset = MAEDataset(path_ann, path_imgs, intersection_threshold=args.intersection_threshold, resize_image=True)
         dataloader = torch.utils.data.DataLoader(dataset, batch_size=8, shuffle=True, num_workers=4)
@@ -92,25 +146,26 @@ def main():
 
     arch='mae_vit_large_patch16'
     model_mae = getattr(models_mae, arch)()
-    if not args.random_init:
-        checkpoint = torch.load(chkpt_dir, map_location=args.device)
-        msg = model_mae.load_state_dict(checkpoint['model'], strict=False)
-        print(msg)
 
-    if args.checkpoint:
-        chkpt_dir = args.checkpoint
-        model_mae = LightningMAE.load_from_checkpoint(chkpt_dir, model=model_mae)
-        model_mae = model_mae.model_mae
+    checkpoint = torch.load(chkpt_dir, map_location=args.device)
+    msg = model_mae.load_state_dict(checkpoint['model'], strict=False)
+    print(msg)
+
+    chkpt_dir = args.checkpoint
+    model_mae = LightningMAE.load_from_checkpoint(chkpt_dir, model=model_mae)
+    model_mae.eval()
+    model_mae.to(args.device)
+    model_mae = model_mae.model_mae
     
-    model = LightningMAE(model_mae, lr=LEARNING_RATE, l1=L1, num_classes=num_classes)
-    if args.device == 'cpu':
-        trainer = pl.Trainer(accumulate_grad_batches=4, logger=True, enable_checkpointing=True, limit_predict_batches=args.bathc_size, \
-            max_epochs=args.epochs, log_every_n_steps=1, accelerator=args.device)
-    else:
-        trainer = pl.Trainer(accumulate_grad_batches=4, logger=True, enable_checkpointing=True, limit_predict_batches=args.bathc_size, \
-            max_epochs=args.epochs, log_every_n_steps=1, accelerator=args.device, devices=1)
+    ref_sum, counts = count_reference(
+        dataloader=dataloader, 
+        model=model_mae, 
+        device=args.device,
+        num_classes=num_classes)
 
-    trainer.fit(model=model, train_dataloaders=dataloader)
+    ref_mean = (ref_sum.T / counts).T
 
+    embeds, imgs, labels = return_img_embed(dataloader_val, model_mae)
+    
 if __name__ == '__main__':
     main()
