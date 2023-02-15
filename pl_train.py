fd2569c0abba256bc215aa6d980f6aab13a65a48
AlvardBarseghyan <barseghyan.at@gmail.com>
2023-02-15 09:33:06 -0500

    aim integration

--- pl_train.py
+++ pl_train.py
@@ -2,10 +2,13 @@ import os
 import torch 
 import argparse
 import pytorch_lightning as pl
+from pytorch_lightning.callbacks import ModelCheckpoint
 
 from loss import ContrastiveLoss
-from dataset import MAEDataset
 import models_mae
+from util.get_dataloader import get_dataloader
+
+from aim.pytorch_lightning import AimLogger
 
 # TODO: integrate aim
 
@@ -17,6 +20,15 @@ class LightningMAE(pl.LightningModule):
         self.l1 = l1
         self.lr = lr
         self.criterion = ContrastiveLoss(num_classes=num_classes, margin=margin)
+        self.aim_logger = AimLogger(
+            experiment='debug_final'
+        )
+        self.checkpoint_callback = ModelCheckpoint(
+            filename='{epoch}',
+            verbose=True,
+            save_last=True,
+            every_n_epochs=5
+        )
         # self.save_hyperparameters(ignore=['model'])
 
     def training_step(self, batch, batch_idx):
@@ -32,6 +44,20 @@ class LightningMAE(pl.LightningModule):
         print(f'Iter: {batch_idx}, pos_loss: {loss[0].item()}, neg_loss = {self.l1} * {loss[1].item()}, loss: {total_loss.item()}')
 
         return total_loss
+    
+    def validation_step(self, batch, batch_idx):
+
+        img = torch.einsum('nhwc->nchw', batch['image'])
+        print("Getting image embeddings...")
+        img_enc, mask, _ = self.model_mae.forward_encoder(img.float(), mask_ratio=0)
+        img_enc = img_enc[:, 1:, :].reshape(-1, img_enc.shape[-1])
+        print("Val loss counting...")
+        loss = self.criterion(img_enc, batch['indices_labels'].reshape(-1))
+        total_loss = loss[0] + self.l1 * loss[1]
+        self.log('val_loss', total_loss)
+        print(f'Iter: {batch_idx}, val_pos_loss: {loss[0].item()}, val_neg_loss = {self.l1} * {loss[1].item()}, val_loss: {total_loss.item()}')
+
+        return total_loss
 
 
     def configure_optimizers(self):
@@ -42,7 +68,6 @@ class LightningMAE(pl.LightningModule):
     def forward(self, img):
         encoded_image, mask, indices = self.model_mae.forward_encoder(img, mask_ratio=0)
         return encoded_image[:, 1:, :]
-
 
 
 def main():
@@ -91,38 +116,16 @@ def main():
         default=False,
         type=bool,
     )
+    parser.add_argument(
+        '--annotation_train'
+    )
+    parser.add_argument(
+        '--annotation_val'
+    )
 
     args = parser.parse_args()
 
-
-    if args.dataset_name.lower() == 'fair1m':
-        root = '/mnt/2tb/hrant/FAIR1M/fair1m_1000/train1000/'
-        path_ann = os.path.join(root, 'few_shot_8.json')
-        path_imgs = os.path.join(root, 'images')
-        dataset = MAEDataset(path_ann, path_imgs, intersection_threshold=args.intersection_threshold, resize_image=True)
-        dataloader = torch.utils.data.DataLoader(dataset, batch_size=args.batch_size, shuffle=True, num_workers=4)
-        num_classes = 5
-
-    elif args.dataset_name.lower() == 'vis_drone' or args.dataset_name.lower() == 'vs':
-        root = '/lwll/development/vis_drone/vis_drone_full/train/'
-        path_ann = os.path.join('./annotations/', 'few_shot_visdrone.json')
-        path_imgs = os.path.join(root, '')
-
-        dataset = MAEDataset(path_ann, path_imgs, intersection_threshold=args.intersection_threshold, resize_image=True)
-        dataloader = torch.utils.data.DataLoader(dataset, batch_size=args.batch_size, shuffle=True, num_workers=4)
-        num_classes = 11
-
-    elif args.dataset_name.lower() == 'nightowls':
-        path_ann = './annotations/few_shot_8_nightowls.json'
-        path_imgs = '/home/ani/nightowls_stage_3/'
-        dataset = MAEDataset(path_ann, path_imgs, intersection_threshold=args.intersection_threshold, resize_image=True)
-        dataloader = torch.utils.data.DataLoader(dataset, batch_size=8, shuffle=True, num_workers=4)
-        num_classes = 3
-
-
-    elif args.dataset_name.lower() == 'city_scapes' or args.dataset_name.lower() == 'cs':
-        pass # ToDo
-
+    dataloader, dataloader_val, num_classes = get_dataloader(args.dataset_name, args.annotation_train, args.annotation_val, args.intersection_threshold, args.batch_size)
     
     if args.server == 'c9':
         chkpt_dir = '/mnt/2tb/hrant/checkpoints/mae_models/mae_visualize_vit_large_ganloss.pth'
@@ -145,13 +148,13 @@ def main():
     
     model = LightningMAE(model_mae, lr=LEARNING_RATE, l1=L1, num_classes=num_classes)
     if args.device == 'cpu':
-        trainer = pl.Trainer(accumulate_grad_batches=4, logger=True, enable_checkpointing=True, limit_predict_batches=args.bathc_size, \
-            max_epochs=args.epochs, log_every_n_steps=1, accelerator=args.device)
+        trainer = pl.Trainer(accumulate_grad_batches=4, logger=model.aim_logger, enable_checkpointing=True, limit_predict_batches=args.batch_size, \
+            max_epochs=args.epochs, log_every_n_steps=1, accelerator=args.device, val_check_interval=5, callbacks=[model.checkpoint_callback])
     else:
-        trainer = pl.Trainer(accumulate_grad_batches=4, logger=True, enable_checkpointing=True, limit_predict_batches=args.bathc_size, \
-            max_epochs=args.epochs, log_every_n_steps=1, accelerator=args.device, devices=1)
+        trainer = pl.Trainer(accumulate_grad_batches=4, logger=model.aim_logger, enable_checkpointing=True, limit_predict_batches=args.batch_size, \
+            max_epochs=args.epochs, log_every_n_steps=1, accelerator=args.device, devices=1, val_check_interval=5, callbacks=[model.checkpoint_callback])
 
-    trainer.fit(model=model, train_dataloaders=dataloader)
+    trainer.fit(model=model, train_dataloaders=dataloader, val_dataloaders=dataloader_val)
 
 if __name__ == '__main__':
     main()
