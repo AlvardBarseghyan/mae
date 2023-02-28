994a1bf817c05edb43bea57a4b6efecc3f60539a
AlvardBarseghyan <barseghyan.at@gmail.com>
2023-02-28 03:45:30 -0500

    added encoder for model to train contrastive, linear or both types

--- pl_train.py
+++ pl_train.py
@@ -1,5 +1,6 @@
 import os 
 import torch 
+import torch.nn as nn
 import argparse
 import pytorch_lightning as pl
 from pytorch_lightning.callbacks import ModelCheckpoint
@@ -15,14 +16,67 @@ import numpy as np
 
 # TODO: integrate aim
 
+class Encoder(pl.LightningModule):
+    def __init__(self, model, num_classes, backbone_freeze=False, classifier='linear'):
+        super().__init__()
+        self.model = model
+        self.classifier = classifier
+        self.backbone_freeze = backbone_freeze
+        # if backbone_freeze:
+        #     print('Frozen backbone')
+        #     for param in self.model.parameters():
+        #         param.requires_grad = False
+
+        if self.classifier == 'linear' or self.classifier == 'both':
+            self.head = nn.Linear(1024, num_classes+1)
+            self.activation = nn.Softmax(dim=-1)
+        
+
+    def forward(self, x, mask_ratio=0):
+        if self.backbone_freeze:
+            self.model.eval()
+            with torch.no_grad():
+                img_enc, mask, _ = self.model.forward_encoder(x, mask_ratio=mask_ratio)
+        else:
+            img_enc, mask, _ = self.model.forward_encoder(x, mask_ratio=mask_ratio)
+
+        img_enc = img_enc[:, 1:, :].reshape(-1, img_enc.shape[-1])
+
+        if self.classifier == 'contrastive':
+            return img_enc
+
+        if self.classifier == 'linear' or self.classifier == 'both':
+            img_enc = self.head(img_enc)
+            img_enc = self.activation(img_enc)
+            return img_enc
+        
+
+
 class LightningMAE(pl.LightningModule):
-    def __init__(self, model, experiment='', l1=0.5, lr=1e-4, num_classes=5, margin=1):
+    def __init__(self, model, weighted=False, loss_type='contrastive', experiment='', l1=0.5, lr=1e-4, num_classes=5, margin=1):
         super().__init__()
         self.model_mae = model
+        self.weighted = weighted
         self.num_classes = num_classes
+        self.loss_type = loss_type
         self.l1 = l1
         self.lr = lr
-        self.criterion = ContrastiveLoss(num_classes=num_classes, margin=margin)
+
+        # if backbone_freeze:
+        #     print('Frozen backbone')
+        #     for param in self.model_mae.parameters():
+        #         param.requires_grad = False
+
+        if loss_type == 'contrastive':
+            self.criterion = ContrastiveLoss(num_classes=num_classes, margin=margin)
+        elif loss_type == 'linear':
+            # self.head = nn.Linear(1024, num_classes+1)
+            self.criterion = nn.CrossEntropyLoss()
+        elif loss_type == 'both':
+            # self.head = nn.Linear(1024, num_classes+1)
+            self.crossentropyloss = nn.CrossEntropyLoss()
+            self.contrastiveloss = ContrastiveLoss(num_classes=num_classes, margin=margin)
+
         self.aim_logger = AimLogger(
             experiment=experiment
         )
@@ -37,37 +91,88 @@ class LightningMAE(pl.LightningModule):
         # self.save_hyperparameters(ignore=['model'])
 
     def training_step(self, batch, batch_idx):
-
         img = torch.einsum('nhwc->nchw', batch['image'])
-        print("Getting image embeddings...")
-        img_enc, mask, _ = self.model_mae.forward_encoder(img.float(), mask_ratio=0)
-        img_enc = img_enc[:, 1:, :].reshape(-1, img_enc.shape[-1])
-        print("Loss counting...")
-        loss = self.criterion(img_enc, batch['indices_labels'].reshape(-1), batch['weighted_labels'].reshape(-1))
-        total_loss = loss[0] + self.l1 * loss[1]
-        distance_matrix = loss[2]
-        aim_image = Image(distance_matrix)
-        self.log('train_loss', total_loss)
-        self.aim_logger.experiment.track(aim_image, "Confusion Matrix")
-        print(f'Iter: {batch_idx}, pos_loss: {loss[0].item()}, neg_loss = {self.l1} * {loss[1].item()}, loss: {total_loss.item()}')
+        img_enc = self.model_mae(img.float(), mask_ratio=0)
 
+        if self.loss_type == 'linear':
+            one_hot_labels = nn.functional.one_hot(batch['indices_labels'].reshape(-1).to(torch.int64), self.num_classes+1)
+            total_loss = self.criterion(img_enc, one_hot_labels.to(torch.float32))
+
+        elif self.loss_type == 'contrastive':
+            if self.weighted:
+                loss = self.criterion(img_enc, batch['indices_labels'].reshape(-1), batch['weighted_labels'].reshape(-1))
+            else:
+                loss = self.criterion(img_enc, batch['indices_labels'].reshape(-1))
+
+            total_loss = loss[0] + self.l1 * loss[1]
+            distance_matrix = loss[2]
+            aim_image = Image(distance_matrix)
+            
+            self.aim_logger.experiment.track(aim_image, "Confusion Matrix")
+
+        elif 'both':
+            one_hot_labels = nn.functional.one_hot(batch['indices_labels'].reshape(-1).to(torch.int64), self.num_classes+1)
+            cross_loss = self.crossentropyloss(img_enc, one_hot_labels.to(torch.float32))
+
+            if self.weighted:
+                loss = self.contrastiveloss(img_enc, batch['indices_labels'].reshape(-1), batch['weighted_labels'].reshape(-1))
+            else:
+                loss = self.contrastiveloss(img_enc, batch['indices_labels'].reshape(-1))
+
+            total_loss = loss[0] + self.l1 * loss[1] + cross_loss
+            
+            distance_matrix = loss[2]
+            aim_image = Image(distance_matrix)
+            
+            self.aim_logger.experiment.track(aim_image, "Confusion Matrix")
+
+
+
+            # print(f'Iter: {batch_idx}, pos_loss: {loss[0].item()}, neg_loss = {self.l1} * {loss[1].item()}, loss: {total_loss.item()}')
+        self.log('train_loss', total_loss)
+        print('Epoch', self.current_epoch)
         return total_loss
     
     def validation_step(self, batch, batch_idx):
 
         img = torch.einsum('nhwc->nchw', batch['image'])
-        print("Getting image embeddings...")
-        img_enc, mask, _ = self.model_mae.forward_encoder(img.float(), mask_ratio=0)
-        img_enc = img_enc[:, 1:, :].reshape(-1, img_enc.shape[-1])
-        print("Val loss counting...")
-        loss = self.criterion(img_enc, batch['indices_labels'].reshape(-1), batch['weighted_labels'].reshape(-1))
-        total_loss = loss[0] + self.l1 * loss[1]
-        distance_matrix = loss[2]
-        print(np.array(distance_matrix).shape)
-        aim_image = Image(distance_matrix)
-        self.aim_logger.experiment.track(aim_image, "Confusion Matrix val")
+        img_enc = self.model_mae(img.float(), mask_ratio=0)
+        if self.loss_type == 'linear':
+            one_hot_labels = nn.functional.one_hot(batch['indices_labels'].reshape(-1).to(torch.int64), self.num_classes+1)
+            total_loss = self.criterion(img_enc, one_hot_labels.to(torch.float32))
+
+        elif self.loss_type == 'contrastive':
+            if self.weighted:
+                loss = self.criterion(img_enc, batch['indices_labels'].reshape(-1), batch['weighted_labels'].reshape(-1))
+            else:
+                loss = self.criterion(img_enc, batch['indices_labels'].reshape(-1))
+
+            total_loss = loss[0] + self.l1 * loss[1]
+            distance_matrix = loss[2]
+            aim_image = Image(distance_matrix)
+            self.aim_logger.experiment.track(aim_image, "Confusion Matrix")
+
+            print(f'Iter: {batch_idx}, pos_loss: {loss[0].item()}, neg_loss = {self.l1} * {loss[1].item()}, loss: {total_loss.item()}')
+
+
+        elif 'both':
+            one_hot_labels = nn.functional.one_hot(batch['indices_labels'].reshape(-1).to(torch.int64), self.num_classes+1)
+            cross_loss = self.crossentropyloss(img_enc, one_hot_labels.to(torch.float32))
+
+            if self.weighted:
+                print('weigtshdcn ', self.weighted)
+
+                loss = self.contrastiveloss(img_enc, batch['indices_labels'].reshape(-1), batch['weighted_labels'].reshape(-1))
+            else:
+                loss = self.contrastiveloss(img_enc, batch['indices_labels'].reshape(-1))
+
+            total_loss = loss[0] + self.l1 * loss[1] + cross_loss
+            distance_matrix = loss[2]
+            aim_image = Image(distance_matrix)
+            
+            self.aim_logger.experiment.track(aim_image, "Confusion Matrix")
+
         self.log('val_loss', total_loss)
-        print(f'Iter: {batch_idx}, val_pos_loss: {loss[0].item()}, val_neg_loss = {self.l1} * {loss[1].item()}, val_loss: {total_loss.item()}')
 
         return total_loss
 
@@ -128,19 +233,19 @@ def main():
         default=False,
         type=bool,
     )
-    parser.add_argument(
-        '--annotation_train'
-    )
-    parser.add_argument(
-        '--annotation_val'
-    )
-    parser.add_argument(
-        '--experiment'
-    )
+    parser.add_argument('--annotation_train')
+    parser.add_argument('--annotation_val')
+    parser.add_argument('--experiment')
+    parser.add_argument('--loss_type', default='contrastive')
+    parser.add_argument('--weighted', default=False, type=bool)
+    parser.add_argument('--backbone_freeze', default=False, type=bool)
 
     args = parser.parse_args()
 
-    dataloader, dataloader_val, num_classes, dataset = get_dataloader(args.dataset_name, args.annotation_train, args.annotation_val, args.intersection_threshold, args.batch_size, True)
+    dataloader, dataloader_val, num_classes, dataset = get_dataloader(dataset_name=args.dataset_name, \
+    train_annotation_file=args.annotation_train, val_annotation_file=args.annotation_val, \
+    intersection_threshold=args.intersection_threshold, batch_size=args.batch_size, \
+    weighted=args.weighted, return_dataset=True)
     
     if args.server == 'c9':
         chkpt_dir = '/mnt/2tb/hrant/checkpoints/mae_models/mae_visualize_vit_large_ganloss.pth'
@@ -161,7 +266,10 @@ def main():
         model_mae = LightningMAE.load_from_checkpoint(chkpt_dir, model=model_mae)
         model_mae = model_mae.model_mae
     
-    model = LightningMAE(model_mae, experiment=args.experiment, lr=LEARNING_RATE, l1=L1, num_classes=num_classes)
+    print(args.weighted, int(args.weighted), args.backbone_freeze, int(args.backbone_freeze))
+    encoder = Encoder(model=model_mae, num_classes=num_classes, backbone_freeze=args.backbone_freeze, classifier=args.loss_type)
+
+    model = LightningMAE(model=encoder, weighted=args.weighted, loss_type=args.loss_type, experiment=args.experiment, lr=LEARNING_RATE, l1=L1, num_classes=num_classes)
     if args.device == 'cpu':
         trainer = pl.Trainer(accumulate_grad_batches=32, logger=model.aim_logger, enable_checkpointing=True, limit_predict_batches=args.batch_size, \
             max_epochs=args.epochs, log_every_n_steps=1, accelerator=args.device, val_check_interval=int(round(len(dataset)/args.batch_size)), callbacks=[model.checkpoint_callback])
